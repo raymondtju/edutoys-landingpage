302ae33955514c6aed84254d395b8146f8cbf6da
raymondtju <raymond.rtju@gmail.com>
2023-03-07 12:07:48 +0700

    responsive layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,12 @@ import React from "react";
 
 function Layout({ children }: any) {
   return (
-    <div className={clsx(`max-w-[1440px] mx-auto px-[112px] overflow-hidden`)}>
+    <div
+      className={clsx(
+        `max-w-[1440px] mx-auto overflow-hidden px-4`,
+        `xl:px-[112px] lg:px-[80px] md:px-[40px] sm:px-[24px]`
+      )}
+    >
       {children}
     </div>
   );
